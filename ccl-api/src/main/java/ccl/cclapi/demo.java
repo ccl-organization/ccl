4ffb31eea6d8b7d2af5a362faaa30af844612c36
陈亚辉 <chenyahui@dookbook.com>
2018-06-23 18:28:08 +0800

    init

---
+++ ccl-api/src/main/java/ccl/cclapi/demo.java
@@ -0,0 +1,10 @@
+package com;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: yahui
+ * Date: 2018/6/23
+ * To change this template use File | Settings | File Templates.
+ **/
+public class demo {
+}
